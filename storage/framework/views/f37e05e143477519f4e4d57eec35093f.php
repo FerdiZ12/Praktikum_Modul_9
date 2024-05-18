1cbc60ccdccd2ab223e1b64ba37d6d72008da1f2
FerdiZ12 <151631517+FerdiZ12@users.noreply.github.com>
2024-05-18 23:51:21 +0700

    Add files via upload

---
+++ storage/framework/views/f37e05e143477519f4e4d57eec35093f.php
@@ -0,0 +1,12 @@
+<div class="container mt-4">
+    <h4><?php echo e($pageTitle); ?></h4>
+    <hr>
+    <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3
+    border">
+        <div class="<?php echo e($icon); ?> me-3 fs-1"></div>
+        <h4 class="mb-0">Well done! this is <?php echo e($pageTitle); ?>.</h4>
+    </div>
+</div>
+
+
+<?php /**PATH C:\Users\Ferdi\Documents\Modul 4\Prak4\resources\views/default.blade.php ENDPATH**/ ?>
